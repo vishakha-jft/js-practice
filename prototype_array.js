2768327a169fe17a4d52f72fdad0ffe3d4b6da4f
vishakha-jft <vishakha.yadav@jellyfishtechnologies.com>
2022-11-17 18:01:30 +0530

    with for each loop

--- prototype_array.js
+++ prototype_array.js
@@ -1,6 +1,17 @@
 Array.prototype.powerOf = function(n) {
-    for (let i = 0; i < this.length; i++) {
-      this[i] = this[i]**n;
-    }
-  };
+    this.forEach( function(elem,idx){
+      this[idx] =  this[idx]**n
+    }, this)
+  }
+
+Array.prototype.rotate = function(n){
+  temp=
+  this.forEach( function(elem, idx){
+    
+  },this)
   
+}
+
+var arr=[1,2,3,4,5,6,7,8];
+  arr.powerOf(3);
+  console.log(arr);
